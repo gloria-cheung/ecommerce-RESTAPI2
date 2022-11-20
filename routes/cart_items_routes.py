b0e43c0ec27f32db7a92c5dcd863fcb4088cbcac
gloria <gloriacheung812@gmail.com>
2022-11-20 17:37:51 -0500

    change such that cart total reflects changes if cart items are added, updated or deleted from cart

--- routes/cart_items_routes.py
+++ routes/cart_items_routes.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify
-from models import db, CartItem, User
+from models import db, CartItem, User, Cart, Product
 from flask_login import current_user, login_required
 from __main__ import app
 
@@ -16,6 +16,8 @@ def add_cart_item_to_cart():
             product_id=int(data.get("product_id")),
             quantity=int(data.get("quantity"))
         )
+        found_cart = Cart.query.get(cart_id)
+        found_cart.total += Product.query.get(int(data.get("product_id"))).price * int(data.get("quantity"))
         db.session.add(new_cart_item)
         db.session.commit()
         return jsonify({"success": "item successfully added to cart"})
@@ -29,9 +31,14 @@ def add_cart_item_to_cart():
 @login_required
 def update_cart_item(cart_item_id):
     try:
+        # find cart item and update qty, then update the cart total based on this change
         cart_item = CartItem.query.get(cart_item_id)
         if not cart_item:
             return jsonify({"error": "could not find cart item based on id given"}), 400
+
+        difference_in_qty = cart_item.quantity - request.json.get("quantity")
+        found_cart = Cart.query.get(cart_item.cart_id)
+        found_cart.total -= cart_item.product.price * difference_in_qty
         cart_item.quantity = request.json.get("quantity")
         db.session.commit()
         return jsonify({"success": "cart item quantity successfully updated"})
@@ -45,9 +52,13 @@ def update_cart_item(cart_item_id):
 @login_required
 def delete_cart_item(cart_item_id):
     try:
+        # find cart item and remove from cart, then update cart total based on this removal
         cart_item = CartItem.query.get(cart_item_id)
         if not cart_item:
             return jsonify({"error": "could not find cart item based on id given"}), 400
+
+        found_cart = Cart.query.get(cart_item.cart_id)
+        found_cart.total -= cart_item.product.price * cart_item.quantity
         db.session.delete(cart_item)
         db.session.commit()
         return jsonify({"success": "cart item deleted"})
