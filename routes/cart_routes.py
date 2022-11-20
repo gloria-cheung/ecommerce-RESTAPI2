4b9a0700762a516d309038e84e835f2d4041ca75
gloria <gloriacheung812@gmail.com>
2022-11-20 17:02:17 -0500

    create routes to create cart, get cart and clear cart

--- routes/cart_routes.py
+++ routes/cart_routes.py
@@ -1,31 +1,53 @@
 from flask import request, jsonify
-from models import db, Cart
-from __main__ import app, admin_only
-from datetime import datetime
+from models import db, Cart, CartItem
+from flask_login import current_user, login_required
+from __main__ import app
 
 
 # get cart
-@app.route('/api/cart/<int:cart_id>')
-def get_cart(cart_id):
-    pass
+@app.route('/api/cart')
+@login_required
+def get_cart():
+    try:
+        found_cart = Cart.query.filter_by(user_id=int(current_user.get_id())).first()
+        if not found_cart:
+            return jsonify({"error": "could not find cart based on user_id"}), 400
+        return jsonify(cart=found_cart.obj_to_dict(), cart_items=[item.obj_to_dict() for item in found_cart.cart_items])
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 # create cart
 @app.route('/api/cart/', methods=["POST"])
-@admin_only
+@login_required
 def create_cart():
-    pass
+    try:
+        new_cart = Cart(
+            user_id=int(current_user.get_id())
+        )
+        db.session.add(new_cart)
+        db.session.commit()
+        return jsonify(new_cart.obj_to_dict())
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
-# update cart
-@app.route('/api/cart/<int:cart_id>', methods=["PATCH"])
-@admin_only
-def update_cart(cart_id):
-    pass
+# clear cart of items
+@app.route('/api/cart/', methods=["PATCH"])
+@login_required
+def update_cart():
+    try:
+        found_cart = Cart.query.filter_by(user_id=int(current_user.get_id())).first()
+        if not found_cart:
+            return jsonify({"error": "could not find cart based on user_id"}), 400
+        cart_items = CartItem.query.filter_by(cart_id=found_cart.id).all()
+        for item in cart_items:
+            db.session.delete(item)
+            db.session.commit()
 
+        return jsonify(cart=found_cart.obj_to_dict(), cart_items=[item.obj_to_dict() for item in found_cart.cart_items])
 
-# delete cart
-@app.route('/api/cart/<int:product_id>', methods=["DELETE"])
-@admin_only
-def delete_cart(cart_id):
-    pass
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
