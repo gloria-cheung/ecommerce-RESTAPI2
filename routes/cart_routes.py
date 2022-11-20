eae21645fd1bdfdccdd4f2d56ae5f9d2fc2fafc0
gloria <gloriacheung812@gmail.com>
2022-11-20 17:38:05 -0500

    change such that cart total reflects changes if cart items are added, updated or deleted from cart

--- routes/cart_routes.py
+++ routes/cart_routes.py
@@ -39,13 +39,18 @@ def create_cart():
 @login_required
 def update_cart():
     try:
+        # find cart, delete all cart items associated with this cart and update cart total to be 0
         found_cart = Cart.query.filter_by(user_id=int(current_user.get_id())).first()
         if not found_cart:
             return jsonify({"error": "could not find cart based on user_id"}), 400
+
         cart_items = CartItem.query.filter_by(cart_id=found_cart.id).all()
         for item in cart_items:
             db.session.delete(item)
             db.session.commit()
+
+        found_cart.total = 0
+        db.session.commit()
 
         return jsonify(cart=found_cart.obj_to_dict(), cart_items=[item.obj_to_dict() for item in found_cart.cart_items])
 
